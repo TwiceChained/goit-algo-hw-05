34824e16fbe71826361611b7bb3d8a3f55ae3f01
Vladyslav Palada <vladyslav.palada@gmail.com>
2025-09-12 15:12:51 +0300

    done 3 tasks out of 4

---
+++ Second.py
@@ -0,0 +1,24 @@
+import re 
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+pattern = r"\d+\.\d+"
+
+def  generator_numbers(out_string):
+    numbers_list = re.findall(pattern, out_string)
+
+    for number in numbers_list:
+        float_number = float(number)
+        yield float_number
+
+
+def sum_profit(out_string, func):
+    generator = func(out_string)
+    return sum(generator)
+   
+
+total = sum_profit(text, generator_numbers)
+print(total)
+
+
+
+
